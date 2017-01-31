ae9b8147287d0b854a2f6ef2968a52707fbca6a0
Jeevan <jeevan8082@gmail.com>
2017-01-31 18:49:16 +0530

    Created Email and Password fileds as customer view

---
+++ IOSFormValidator/IOSFormValidator/EmainView.m
@@ -0,0 +1,50 @@
+//
+//  EmainView.m
+//  IOSFormValidator
+//
+//  Created by Jeevan on 31/01/17.
+//  Copyright © 2017 com.byjeevan.ios.formvalidator. All rights reserved.
+//
+
+#import "EmainView.h"
+
+@implementation EmainView
+
+
+-(id)initWithCoder:(NSCoder *)coder {
+    
+    self = [super initWithCoder:coder];
+    
+    if (self) {
+     
+    }
+
+    return self;
+}
+
+
+-(id) initWithFrame:(CGRect)frame{
+    
+    self = [super initWithFrame:frame];
+    
+    if (self) {
+        
+    }
+    
+    return self;
+}
+
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+    if (self) {
+        
+        UITextField * emailTextView = [[UITextField alloc] initWithFrame:rect];
+        emailTextView.backgroundColor = [UIColor yellowColor];
+        [self addSubview:emailTextView];
+        
+    }
+}
+
+@end
---
+++ IOSFormValidator/IOSFormValidator/PasswordView.h
@@ -0,0 +1,13 @@
+//
+//  PasswordView.h
+//  IOSFormValidator
+//
+//  Created by Jeevan on 31/01/17.
+//  Copyright © 2017 com.byjeevan.ios.formvalidator. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PasswordView : UIView
+
+@end
