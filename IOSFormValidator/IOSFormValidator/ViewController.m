88c0e75ed47ee43d433bf94f5def643d45034768
Jeevan <Axio-Mac@AXN-116.local>
2017-01-31 17:39:29 +0530

    Initial commit

---
+++ IOSFormValidator/IOSFormValidator/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  IOSFormValidator
+//
+//  Created by Jeevan on 31/01/17.
+//  Copyright © 2017 com.byjeevan.ios.formvalidator. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ IOSFormValidator/IOSFormValidator/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  IOSFormValidator
+//
+//  Created by Jeevan on 31/01/17.
+//  Copyright © 2017 com.byjeevan.ios.formvalidator. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
