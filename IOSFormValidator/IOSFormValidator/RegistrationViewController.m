594883d7a71e3d7d1adbe0a20d12689c458cc587
byJeevan <jeevan8082@gmail.com>
2017-02-02 22:32:04 +0530

    Design - Registration controller views [2]

--- IOSFormValidator/IOSFormValidator/RegistrationViewController.m
+++ IOSFormValidator/IOSFormValidator/RegistrationViewController.m
@@ -10,15 +10,45 @@
 
 @interface RegistrationViewController ()
 
+@property (weak, nonatomic) IBOutlet UITextField *firstNameField;
+@property (weak, nonatomic) IBOutlet UIButton *errorIconFirstName;
+@property (weak, nonatomic) IBOutlet UILabel *errorLabelFirstName;
+
+@property (weak, nonatomic) IBOutlet UITextField *lastName;
+@property (weak, nonatomic) IBOutlet UIButton *errorIconLasteName;
+@property (weak, nonatomic) IBOutlet UILabel *errorLabelLastName;
+
+@property (weak, nonatomic) IBOutlet UISegmentedControl *genderSegment;
+
+@property (weak, nonatomic) IBOutlet UITextField *emailField;
+@property (weak, nonatomic) IBOutlet UIButton *emailErrorIcon;
+@property (weak, nonatomic) IBOutlet UILabel *emailErrorLabel;
+
+@property (weak, nonatomic) IBOutlet UITextField *mobileField;
+
+@property (weak, nonatomic) IBOutlet UILabel *mobileErrorLabel;
+
+@property (weak, nonatomic) IBOutlet UIButton *mobileErrorIcon;
+
+@property (weak, nonatomic) IBOutlet UITextField *passwordField;
+
+@property (weak, nonatomic) IBOutlet UILabel *passwordErrorLabel;
+@property (weak, nonatomic) IBOutlet UIButton *passwordErrorIcon;
+@property (weak, nonatomic) IBOutlet UISwitch *agreementSwitch;
+
 @end
 
 @implementation RegistrationViewController
+- (IBAction)backAction:(id)sender {
+}
+- (IBAction)submitAction:(id)sender {
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
    
-    
-    
+    //Make placeholder color white
+ 
 }
 
 - (void)didReceiveMemoryWarning {
